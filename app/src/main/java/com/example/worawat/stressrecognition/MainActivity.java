2a83caf280d3c398695cc8bc02fe4e5374c6843b
worawatty <worawat.law@gmail.com>
2017-10-17 22:39:15 +0900

    Test Git

--- app/src/main/java/com/example/worawat/stressrecognition/MainActivity.java
+++ app/src/main/java/com/example/worawat/stressrecognition/MainActivity.java
@@ -22,6 +22,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+//edit this activity for stress recognition
+
 public class MainActivity extends AppCompatActivity implements QuestionFragment.OnFragmentInteractionListener, View.OnClickListener, UpdateSASScore.AsyncResponseScore {
     ArrayList<String[]> questions;
     ArrayList<int[]> answer;
